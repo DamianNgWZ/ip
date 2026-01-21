29a6045ba2dcd2c5c2821b898a72030e2baf0db1
Ng Wei Zong Damian <damianng87@gmail.com>
2026-01-22 00:15:03 +0800

    feat: add echo support for bot

--- src/main/java/Dbot.java
+++ src/main/java/Dbot.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class Dbot {
     public static void main(String[] args) {
         String botName = "Dbot";
-        System.out.println("Hello! I'm" + botName);
-        System.out.println("What can I do for you?\n Bye. Hope to see you soon again!");
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println("Hello! I'm " + botName);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine().trim();
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+        }
+        sc.close();
     }
 }
