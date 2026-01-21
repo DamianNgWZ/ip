212a6e56acb4720d73521053a567611b4bdf765b
Ng Wei Zong Damian <damianng87@gmail.com>
2026-01-21 23:54:47 +0800

    Rename main class and add greeting

---
+++ src/main/java/Dbot.java
@@ -0,0 +1,7 @@
+public class Dbot {
+    public static void main(String[] args) {
+        String botName = "Dbot";
+        System.out.println("Hello! I'm" + botName);
+        System.out.println("What can I do for you?\n Bye. Hope to see you soon again!");
+    }
+}
