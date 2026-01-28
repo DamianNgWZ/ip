375693db46a727974b8dae27b7e8bbeeceaf58af
Ng Wei Zong Damian <damianng87@gmail.com>
2026-01-28 23:21:42 +0800

    refactor: update bot name back to Dbot in ui

--- src/main/java/dbot/ui/Ui.java
+++ src/main/java/dbot/ui/Ui.java
@@ -12,7 +12,7 @@ public class Ui {
 
     public void showWelcome() {
         showLine();
-        System.out.println("Hello! I'm dbot.Dbot");
+        System.out.println("Hello! I'm Dbot");
         System.out.println("What can I do for you?");
         showLine();
     }
